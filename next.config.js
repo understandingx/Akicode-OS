4f05d9a3d8efdb39184ecf339a5eb498cfe863f9
Victor Ezeigwe <137442785+understandingx@users.noreply.github.com>
2026-05-23 18:09:57 +0200

    chore: add next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.weatherapi.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
